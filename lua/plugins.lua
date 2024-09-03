863ca7cd127620cd85fcabd2a47a72a949a16cfb
okcompiler <139511195+okcompiler@users.noreply.github.com>
2024-09-03 09:48:08 -0500

    Add vim-bundler and vim-rails plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,6 +14,8 @@ return {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
   { 'tpope/vim-repeat' },
+  { 'tpope/vim-bundler' },
+  { 'tpope/vim-rails' },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
